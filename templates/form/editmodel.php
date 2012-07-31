f055a83ab0064ad52845fcf270940f85ef094432
Marcos Minond <minond.marcos@gmail.com>
2012-07-30 21:49:45 -0600

    forms now check for primary keys

--- templates/form/editmodel.php
+++ templates/form/editmodel.php
@@ -6,6 +6,7 @@ element('form/field');
 // field def defaults
 $defs = (object) array(
 	'id' => rand(),
+	'hidden' => 'hidden',
 	'type' => 'text',
 	'value' => ''
 );
@@ -28,6 +29,7 @@ $values = $data->getdata();
 $type = $data->gettype();
 $schema = FabricoModel::getinfo($type);
 $fielddata = FabricoModel::getfielddata($type);
+$primarykey = $schema->primary_key;
 
 // field order information
 // priority follows: parameter, model, table schema
@@ -40,6 +42,10 @@ if (!count($order)) {
 	}
 }
 
+function genname ($name) {
+	return ucwords(preg_replace('/_/', ' ', $name));
+};
+
 // go through each field
 foreach ($order as $field) {
 	$info = new stdClass;
@@ -49,12 +55,17 @@ foreach ($order as $field) {
 	$userdef = array_key_exists($field, $fielddata->editing) ?
 	           $fielddata->editing[ $field ] : array();
 
+	if ($field === $primarykey) {
+		$info->type = $defs->hidden;
+	}
+	else {
+		$info->type = isset($userdef[ $prop->type ]) ? $userdef[ $prop->type ] : $defs->type;
+	}
 	$info->name = $field;
 	$info->value = property_exists($values, $field) ? $values->{ $field } : $defs->value;
-	$info->label = isset($userdef[ $prop->label ]) ? $userdef[ $prop->label ] : ucwords($field);
-	$info->type = isset($userdef[ $prop->type ]) ? $userdef[ $prop->type ] : $defs->type;
+	$info->label = isset($userdef[ $prop->label ]) ? $userdef[ $prop->label ] : genname($field);
 	$info->id = isset($userdef[ $prop->id ]) ? $userdef[ $prop->id ] : $info->name . $defs->id;
-	$info->hidden = $info->type === 'hidden';
+	$info->hidden = $info->type === $defs->hidden;
 	
 	// save this field
 	$fieldlist[] = $info;
@@ -74,4 +85,6 @@ foreach ($order as $field) {
 			)) ?>
 		<? endif ?>
 	<? endforeach ?>
+	<?= br(3) ?>
+	<?= form\submitbutton::gen('Save') ?>
 <?= form\method::close($method, __HTML__) ?>
