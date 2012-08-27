1865d62f3681919ec3bd5c6511595e483f62a9a3
Marcos Minond <minond.marcos@gmail.com>
2012-08-26 23:02:28 -0600

    updating method tag

--- elements/action/ui.php
+++ elements/action/ui.php
@@ -22,24 +22,47 @@ class method extends \Fabrico\Element {
 	const PROP = '": "';
 	const UPDATES = 'update';
 	const FORMDATA = 'formdata';
+	const SELECTOR = 'selector';
+	const ON = 'on';
+	const DATASET = 'dataset';
 
 	/**
 	 * controller method call code
 	 */
 	const METHOD_CALL = <<<JS
+Fabrico.controller.method(
+	"#{method}",
+	[ #{parameters} ],
+	[ #{updates} ],
+	{ #{envargs} },
+	#{callback},
+	#{errback}
+);
+JS;
+
+	/**
+	 * event definition
+	 */
+	const FUNC_DEF = <<<JS
 var #{name} = function () {
-	return Fabrico.controller.method(
-		"#{method}",
-		[ #{parameters} ],
-		[ #{updates} ],
-		{ #{envargs} },
-		#{callback},
-		#{errback}
-	);
+	return #{method_call}
 };
 JS;
 
+
+	/**
+	 * event listener
+	 */
+	const EVENT_BIND = <<<JS
+$("#{selector}").live("#{event}", function (ev) {
+	ev.preventDefault();
+	#{method_call}
+});
+JS;
+
 	public static function pregen (& $args) {
+		$fn = '';
+		$onready = false;
 		$envargs = [];
 		$uniqueenv = [];
 		$parameters = [];
@@ -47,7 +70,12 @@ JS;
 
 		if (isset($args[ self::A_PARAM ])) {
 			foreach ($args[ self::A_PARAM ] as $param) {
-				if (isset($param[ self::FORMDATA ])) {
+				if (isset($param[ self::DATASET ])) {
+					$parameters[] = '$(' . 
+						(isset($param[ self::SELECTOR ]) ? self::QUOTE . $param[ self::SELECTOR ] . self::QUOTE : 'this') .
+						').data("' . $param[ self::DATASET ] . '")';
+				}
+				else if (isset($param[ self::FORMDATA ])) {
 					$parameters[] = 'Fabrico.helper.form2args("#' . $param[ self::FORMDATA ] . '")';
 				}
 				else if (!isset($param[ self::ASSIGNTO ])) {
@@ -78,20 +106,33 @@ JS;
 			$parameters[] = trim($args[ self::A_CONTENT ]);
 		}
 
-		\Fabrico\Page::include_javascript(
-			\Fabrico\Merge::parse(self::METHOD_CALL, [
-				'name' => $args[ self::NAME ],
-				'method' => $args[ self::ACTION ],
-				'parameters' => implode(self::COMMA, $parameters),
-				'updates' => implode(self::COMMA, $updates),
-				'envargs' => implode(self::COMMA, $envargs),
-				'callback' => isset($args[ self::CALLBACK ]) ? 
-				              $args[ self::CALLBACK ] : self::A_NULL,
-				'errback' => isset($args[ self::ERRBACK ]) ? 
-				              $args[ self::ERRBACK ] : self::A_NULL
-			]
-		), true);
+		$fn = \Fabrico\Merge::parse(self::METHOD_CALL, [
+			'method' => $args[ self::ACTION ],
+			'parameters' => implode(self::COMMA, $parameters),
+			'updates' => implode(self::COMMA, $updates),
+			'envargs' => implode(self::COMMA, $envargs),
+			'callback' => isset($args[ self::CALLBACK ]) ? 
+			              $args[ self::CALLBACK ] : self::A_NULL,
+			'errback' => isset($args[ self::ERRBACK ]) ? 
+			              $args[ self::ERRBACK ] : self::A_NULL
+		]);
 
+		if (isset($args[ self::NAME ])) {
+			$fn = \Fabrico\Merge::parse(self::FUNC_DEF, [
+				'name' => $args[ self::NAME ],
+				'method_call' => $fn
+			]);
+		}
+		else if (isset($args[ self::ON ]) && isset($args[ self::SELECTOR ])) {
+			$onready = true;
+			$fn = \Fabrico\Merge::parse(self::EVENT_BIND, [
+				'selector' => $args[ self::SELECTOR ],
+				'event' => $args[ self::ON ],
+				'method_call' => $fn
+			]);
+		}
+
+		\Fabrico\Page::include_javascript($fn, true, $onready);
 		return false;
 	}
 }
