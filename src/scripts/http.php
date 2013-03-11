650adcb002d4628aa9196bb817fd4ec04dbd87e2
Marcos Minond <minond.marcos@gmail.com>
2013-03-10 20:21:06 -0600

    updated http runner

--- src/scripts/http.php
+++ src/scripts/http.php
@@ -4,30 +4,41 @@ require 'autoload.php';
 
 use Fabrico\Core\Application;
 use Fabrico\Response\HttpResponse;
+use Fabrico\Request\HttpRequest;
 
 call_user_func(function() {
 	$app = new Application;
+	$req = new HttpRequest;
 	$res = new HttpResponse;
+	$par = $_REQUEST;
 
-	$par = (object) $_REQUEST;
+	$app->setRoot('/home/server/' . $par['_project']);
 
-	// parse project information
-	$app->setRoot('/home/server/' . $par->_project);
-	unset($par->_project);
+	// build the request object
+	if (isset($par['_controller'])) {
+		$req->setController($par['_controller']);
 
-	// parse view information
-	// $req->setViewFile($_REQUEST['_view']);
-	// unset($_REQUEST['_view']);
+		if (isset($par['_method'])) {
+			$req->setMethod($par['_method']);
+		} else if (isset($par['_action'])) {
+			$req->setAction($par['_action']);
+		}
+	} else if (isset($par['_view'])) {
+		$req->setViewFile($par['_view']);
+	}
 
-	// parse request information
-	// $req->setRequestData($_REQUEST);
+	// clean up the parameters object
+	unset($par['_project']);
+	unset($par['_view']);
+	unset($par['_controller']);
+	unset($par['_action']);
+	unset($par['_method']);
+	$req->setData($par);
 
-	// save request and route to view
-	// if ($req->valid()) {
-		// die("Routing request");
-	// } else {
-		// die("Invalid request");
-	// }
-
-	echo("S");
+	if ($req->valid()) {
+		print_r($req);
+		die("Routing request");
+	} else {
+		die("Invalid request");
+	}
 });
