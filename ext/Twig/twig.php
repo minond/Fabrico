9ac5748f99036d2a82946ca83ad0669f3ae040e0
Marcos Minond <minond.marcos@gmail.com>
2013-04-18 21:23:54 -0600

    added Twig extension

---
+++ ext/Twig/twig.php
@@ -0,0 +1,19 @@
+<?php
+
+use Fabrico\Event\Listener;
+use Fabrico\Event\Reporter;
+
+Reporter::observe('Fabrico\View\View', 'render', Listener::PRE,
+    function($info) {
+        Twig_Autoloader::register();
+
+        $load = new Twig_Loader_Filesystem($info->dirpath);
+        $twig = new Twig_Environment($load);
+
+        $info->content = $twig
+            ->loadTemplate($info->filename)
+            ->render($info->data);
+
+        unset($twig);
+    }
+);
