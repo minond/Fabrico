44f42f93360b54a675d6b9d38cf7e1dd74fe671c
Marcos Minond <minond.marcos@gmail.com>
2013-05-05 11:43:55 -0600

    adding project template

---
+++ bin/project/views/404.php
@@ -0,0 +1,47 @@
+<style type="text/css">
+@font-face {
+    font-family: "Perfect DOS VGA 437";
+    src: url("/Propositum/resources/font/Perfect DOS VGA 437.ttf") format("truetype");
+}
+
+body {
+    background-color: #0102ac;
+    font-size: 19px;
+    font-family: "Perfect DOS VGA 437", monospace;
+    color: white;
+    cursor: default;
+
+    /* user select */
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+center > div {
+    margin-top: 80px;
+    width: 900px;
+    text-align: left;
+}
+
+div.i {
+    background-color: #ababa7;
+    width: 87px;
+    text-align: center;
+    padding: 4px 12px;
+    margin-bottom: 30px;
+}
+</style>
+<center>
+    <div>
+        <center>
+            <div class="i">Internet</div>
+        </center>
+        <p>An invalid URL has been requested and the Internet has been shut down to prevent damage.</p>
+        <p>If this is the first time you've seen this stop error screen, restart the World Wide Web. If this screen appears again, follow these steps:</p>
+        <p>Go to a real page.</p>
+    </div>
+<center>
+
