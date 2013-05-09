6f0f53ad1020cb2c4a65dcb51e39fcc2de2f8141
Marcos Minond <minond.marcos@gmail.com>
2013-05-08 21:03:59 -0600

    working on transfering yaml configuration file into json format

--- lib/Reader/Yaml.php
+++ lib/Reader/Yaml.php
@@ -11,11 +11,16 @@ abstract class Yaml
 {
     public static function parse($file)
     {
+        if (strpos($file, '{') === 0 || strpos($file, '[') === 0) {
+            return json_decode($file, true);
+        }
+
         return Y::parse($file);
     }
 
     public static function dump($obj)
     {
+        // return json_encode($obj, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
         return Y::dump($obj, 100, 2);
     }
 }
