1373bd4ac5345ba19ab14e0f2c954386ff1c5af0
Marcos Minond <minond.marcos@gmail.com>
2013-05-11 20:05:09 -0600

    twig extension clean up

--- ext/Twig/twig.php
+++ ext/Twig/twig.php
@@ -2,20 +2,18 @@
 
 use Fabrico\Event\Reporter;
 use Fabrico\Core\Ext;
+use Twig_Loader_Filesystem as Loader;
+use Twig_Environment as Twig;
 
 if (Ext::enabled('twig')) {
     Reporter::before('fabrico.view.view:render', function($info) {
-        Twig_Autoloader::register();
-
         if ($info->extension === Ext::config('twig:extension')) {
-            $load = new Twig_Loader_Filesystem($info->dirpath);
-            $twig = new Twig_Environment($load);
+            $load = new Loader($info->dirpath);
+            $twig = new Twig($load);
 
             $info->content = $twig
                 ->loadTemplate($info->filename)
                 ->render($info->data);
-
-            unset($twig);
         }
     });
 }
