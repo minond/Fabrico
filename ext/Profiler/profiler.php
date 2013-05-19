ea45e7539ecfcfd49ca8e7627193d5bc6b21b397
Marcos Minond <minond.marcos@gmail.com>
2013-05-18 22:24:53 -0600

    updated to Profiler extension

--- ext/Profiler/profiler.php
+++ ext/Profiler/profiler.php
@@ -1,12 +1,17 @@
 <?php
 
+use Fabrico\Core\Ext;
 use util\profile\Profiler;
 use util\profile\Snapshot;
+use util\profile\reports\Chart;
 
-$profiler = new Profiler('Project Profiler');
-
-$profiler->on('stop', function(Snapshot $snapshot) {
-    print_r($snapshot);
-});
-
-$profiler->start();
+if (Ext::enabled('profiler') && Profiler::enabled()) {
+    $profiler = new Profiler('Project Profiler');
+    $profiler->start();
+    $profiler->on('stop', function(Snapshot $snapshot) {
+        $chart = new Chart;
+        $chart->prepare($snapshot);
+        $chart->configure(['chart_type' => 'LineChart']);
+        echo $chart->output();
+    });
+}
