87086e4ab510a37855373dd59e3a1597fa41e164
Marcos Minond <minond.marcos@gmail.com>
2013-06-14 19:03:22 -0600

    renaming sample controller and adding correct annotations

--- bin/project/controller/Hello.php
+++ bin/project/controller/Hello.php
@@ -5,8 +5,11 @@ namespace %Namespace%\Controller;
 use Fabrico\Request\Request;
 use Fabrico\Response\Response;
 
-class Sample
+class Hello
 {
+    /**
+     * @public
+     */
     public function index(Request $req, Response $res)
     {
         return 'Hello, World';
