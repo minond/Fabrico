05c1a0d414741fb6c40de71e0ff18b6d2981930e
Marcos Minond <minond.marcos@gmail.com>
2013-11-19 22:06:57 -0700

    adding viewconfig command

---
+++ src/Fabrico/Command/StdCommands/ViewConfigCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Fabrico\Command\StdCommands;
+
+use Fabrico\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\TableHelper;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * view configuration items
+ */
+class ViewConfigCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('config');
+        $this->setDescription('View application configuration');
+
+        $this->addOption(
+            'get', 'g',
+            InputOption::VALUE_OPTIONAL,
+            'Retrive a configuration value'
+        );
+
+        $this->addOption(
+            'php', 'p',
+            InputOption::VALUE_NONE,
+            'Output as php'
+        );
+
+        $this->addOption(
+            'json', 'j',
+            InputOption::VALUE_NONE,
+            'Output as json'
+        );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $get = $input->getOption('get');
+
+        if ($get) {
+            $val = $this->conf->get($get);
+
+            if ($input->getOption('php')) {
+                $val = var_export($val, true);
+            } else if ($input->getOption('json')) {
+                $val = json_encode($val, JSON_PRETTY_PRINT);
+            } else {
+                $val = Yaml::dump($val, 100, 2);
+                preg_match("/\n/", $val, $matches);
+                $output->write("Value for <info>{$get}</info>: ");
+
+                if (count($matches)) {
+                    $output->writeln('');
+                    $output->writeln('');
+                }
+            }
+
+            $output->writeln(trim($val));
+        }
+    }
+}
+
