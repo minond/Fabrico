736860a042318d900d75ff48a99ae7a2187da9c6
Marcos Minond <minond.marcos@gmail.com>
2013-12-16 23:42:17 -0700

    moving model class generator into framework

---
+++ src/Fabrico/Model/Model.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Fabrico\Model;
+
+use PDO;
+use Exception;
+use Fabrico\Application;
+use Efficio\Dataset\Model as BaseModel;
+use Efficio\Dataset\Storage\Model\Storage;
+use Efficio\Dataset\Storage\Model\DatabaseStorage;
+use Efficio\Dataset\Storage\Model\FileStorage;
+
+// --------------------------------------
+// lasciate ogne speranza, voi ch'intrate
+// --------------------------------------
+
+Application::call(function() {
+    $key = 'db:' . FABRICO_APP_ENV;
+
+    switch ($this->conf->get("$key:type")) {
+        case 'file':
+            class Model extends BaseModel
+            {
+                use FileStorage;
+            }
+
+            Model::setDirectory(
+                $this->conf->get("$key:flat")
+            );
+
+            break;
+
+        case 'pdo':
+            class Model extends BaseModel
+            {
+                use DatabaseStorage;
+            }
+
+            Model::setConnection(
+                new PDO($this->conf->get("$key:dsn"), null, null, [
+                    // todo cannot be hard-coded
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                ])
+            );
+
+            break;
+
+        default:
+            throw new Exception('Invalid db configuration');
+    }
+});
+
