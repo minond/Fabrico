16a1c1123fe6c5d54f2c55974f3f2112747fc903
Marcos Minond <minond.marcos@gmail.com>
2014-03-16 13:05:42 -0600

    treating app/assets as public dir

--- src/Fabrico/Command/scripts/router.php
+++ src/Fabrico/Command/scripts/router.php
@@ -8,7 +8,7 @@ $uri = $_SERVER['REQUEST_URI'];
 stdout("%s $uri", [yellow('ROUTING')]);
 
 // static resource?
-if (strpos($uri, '/public') === 0) {
+if (strpos($uri, '/public') === 0 || strpos($uri, '/app/assets') === 0) {
     // for file types php can handle out of the box see documentation
     // http://php.net/manual/en/features.commandline.webserver.php
     stdout("%s $uri", [green('RESOURCE')]);
