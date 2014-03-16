ad44f84e7dd44d018b0f32a554d818e45424c71e
Marcos Minond <minond.marcos@gmail.com>
2014-03-16 13:29:18 -0600

    no more public cli functions

--- src/Fabrico/Command/scripts/clioutput.php
+++ src/Fabrico/Command/scripts/clioutput.php
@@ -1,54 +1,57 @@
 <?php
 
-/**
- * cli output
- * format: [Sat Dec 21 21:54:56 2013] addr:port Invalid request (...)
- * @param string $msg
- * @param array $args, merge string aruments (for sprintf)
- * @return int|boolean, number of bytes writen, or false on error
- */
-function stdout($msg, array $args = [])
+class CLI
 {
-    $stdout = fopen('php://stdout', 'w');
-    $tmpl = "[%s] %s:%s %s\n";
+    /**
+     * cli output
+     * format: [Sat Dec 21 21:54:56 2013] addr:port Invalid request (...)
+     * @param string $msg
+     * @param array $args, merge string aruments (for sprintf)
+     * @return int|boolean, number of bytes writen, or false on error
+     */
+    public static function stdout($msg, array $args = [])
+    {
+        $stdout = fopen('php://stdout', 'w');
+        $tmpl = "[%s] %s:%s %s\n";
 
-    $date = date(sprintf('D M %2s H:i:s Y', date('j')));
-    $addr = $_SERVER['SERVER_NAME'];
-    $port = $_SERVER['SERVER_PORT'];
+        $date = date(sprintf('D M %2s H:i:s Y', date('j')));
+        $addr = $_SERVER['SERVER_NAME'];
+        $port = $_SERVER['SERVER_PORT'];
 
-    // new message
-    array_unshift($args, $msg);
-    $msg = call_user_func_array('sprintf', $args);
-    fwrite($stdout, sprintf($tmpl, $date, $addr, $port, $msg));
-}
+        // new message
+        array_unshift($args, $msg);
+        $msg = call_user_func_array('sprintf', $args);
+        fwrite($stdout, sprintf($tmpl, $date, $addr, $port, $msg));
+    }
 
-/**
- * return a yellow string
- * @param string $str
- * @return string
- */
-function yellow($str)
-{
-    return colorstr('1;33', $str);
-}
+    /**
+     * return a yellow string
+     * @param string $str
+     * @return string
+     */
+    public static function yellow($str)
+    {
+        return self::colorstr('1;33', $str);
+    }
 
-/**
- * return a yellow string
- * @param string $str
- * @return string
- */
-function green($str)
-{
-    return colorstr('1;32', $str);
-}
+    /**
+     * return a yellow string
+     * @param string $str
+     * @return string
+     */
+    public static function green($str)
+    {
+        return self::colorstr('1;32', $str);
+    }
 
-/**
- * return a colored string string
- * @param string $color
- * @param string $str
- * @return string
- */
-function colorstr($color, $str)
-{
-    return sprintf("\033[%sm%s\033[0m", $color, $str);
+    /**
+     * return a colored string string
+     * @param string $color
+     * @param string $str
+     * @return string
+     */
+    public static function colorstr($color, $str)
+    {
+        return sprintf("\033[%sm%s\033[0m", $color, $str);
+    }
 }
--- src/Fabrico/Command/scripts/router.php
+++ src/Fabrico/Command/scripts/router.php
@@ -5,16 +5,16 @@ require 'clioutput.php';
 
 // routing info
 $uri = $_SERVER['REQUEST_URI'];
-stdout("%s $uri", [yellow('ROUTING')]);
+CLI::stdout("%s $uri", [CLI::yellow('ROUTING')]);
 
 // static resource?
 if (strpos($uri, '/public') === 0 || strpos($uri, '/app/assets') === 0) {
     // for file types php can handle out of the box see documentation
     // http://php.net/manual/en/features.commandline.webserver.php
-    stdout("%s $uri", [green('RESOURCE')]);
+    CLI::stdout("%s $uri", [CLI::green('RESOURCE')]);
     return false;
 } else {
     $res = require 'scripts/http.php';
     $ret = $res->getStatusCode();
-    stdout("%s [%s] $uri", [green('PROCESSED'), $ret]);
+    CLI::stdout("%s [%s] $uri", [CLI::green('PROCESSED'), $ret]);
 }
